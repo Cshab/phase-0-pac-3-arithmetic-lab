696a20765b3a7f4ac60d50123021ad15c3355bf9
Colby Shabel <jcshabel@gmail.com>
2022-09-13 16:51:08 -0500

    commit tree e38a08de0f85b9424fcd83321282f220a58c2370

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+return a+b 
+}
+add(1000,139)
+function subtract(a,b){
+return a-b
+}
+subtract(20,513)
+function multiply(a,b){
+return a*b
+}
+multiply(1,274929)
+function divide(a,b){
+return a/b
+}
+divide(0/10.612903225806452)
+
+function increment(n){
+   
+    return ++n
+}
+increment(771)
+
+function decrement(n){
+    return --n
+}
+decrement(390)
+
+function makeInt(n){
+
+    return parseInt(n,10)
+}
+makeInt('9000')
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
